9444a7da0748691d5585a23281114770517fe809
David Shweizer <d.a.schweizer@gmail.com>
2018-10-09 13:59:28 +0200

    saving private Matrix

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 
 namespace test1
 {
@@ -6,7 +8,37 @@ namespace test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            sw.AutoFlush = true;
+            Console.SetOut(sw);
+            TekAreaDef T1 = new TekAreaDef(new Point(3, 4));
+            T1.Dump(sw);
+            TekAreaDef T2 = new TekAreaDef(new Point(3, 4), new Point(1, 0), new Point(2, 0));
+            T2.Dump(sw);
+            TekAreaDef T3 = new TekAreaDef(new Point(3, 4), new Point(1, 0), new Point(2, 0),
+                new Point(3, 0), new Point(3, 1), new Point(4, 0)
+                );
+            T3.Dump(sw);
+            TekAreaDef T4 = new TekAreaDef(new Point(3, 4), new Point(0, 1), new Point(0, 2),
+                new Point(0, 3), new Point(-1, 3), new Point(0, 4)
+                );
+            T4.Dump(sw);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            TekAreaDef T5 = T3.Rotate90();
+            TekAreaDef T6 = T5.Rotate90();
+            TekAreaDef T7 = T6.Rotate90();
+            TekAreaDef T8 = T7.Rotate90();
+            T3.Dump(sw);
+            T5.Dump(sw);
+            T6.Dump(sw);
+            T7.Dump(sw);
+            T8.Dump(sw);
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ test1/TekAreaDef.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace test1
+{
+    public class TekAreaDef
+    {
+        const int MAXTEK = 5;
+        Point TopLeft;
+        List<Point> Deltas;
+        int xSize, ySize;
+
+        public TekAreaDef(Point topLeft, params Point[] deltas)
+        {
+            TopLeft = topLeft;
+            Deltas = new List<Point>();
+            foreach(Point value in deltas)
+            {
+                if (Deltas.Count == MAXTEK)
+                    break;                    
+                Deltas.Add(value);
+            }
+            ComputeSize();
+        }
+
+        private void ComputeSize()
+        {
+            if (Deltas.Count()==0)
+            {
+                xSize = 1;
+                ySize = 1;
+                return;
+            }
+            int xMin = MAXTEK, yMin = MAXTEK, xMax = 0, yMax = 0;
+            foreach(Point value in Deltas)
+            {
+                if (value.X < xMin)
+                    xMin = value.X;
+                if (value.X > xMax)
+                    xMax = value.X;
+                if (value.Y < yMin)
+                    yMin = value.Y;
+                if (value.Y > yMax)
+                    yMax = value.Y;
+            }
+            xSize = 1 + xMax - xMin;
+            ySize = 1 + yMax - yMin;
+        }
+
+        public TekAreaDef Rotate90()
+        {
+            TekAreaDef result = new TekAreaDef(this.TopLeft);
+            foreach(Point value in this.Deltas)
+            {
+                result.Deltas.Add(new Point(-value.Y, value.X));
+            }
+            result.ComputeSize();
+            return result;
+        }
+
+        public void Dump(StreamWriter sw)
+        {
+            sw.Write(String.Format("Area: [{0},{1} (size:({2},{3})]", TopLeft.X, TopLeft.Y, xSize, ySize));
+            foreach(Point value in Deltas)
+            {
+                sw.Write(" ({0},{1})", value.X, value.Y);
+            }
+            sw.WriteLine();
+        }
+    }
+
+    public class SimpleMatrix
+    {
+        private double[,] values;
+        public int xSize { get { return values.GetLength(0); } }
+        public int ySize { get { return values.GetLength(1); } }
+
+        public double GetValue(int i, int j)
+        {
+            return values[i, j];
+        }
+        public void SetValue(int i, int j, double value)
+        {
+            values[i, j] = value;
+        }
+
+        public SimpleMatrix(int xSize, int ySize)
+        {
+            values = new double[xSize, ySize];
+        }
+
+        public SimpleMatrix Multiply(SimpleMatrix matrix)
+        {
+            SimpleMatrix result = new SimpleMatrix(this.xSize, matrix.ySize);
+            for (int i = 0; i < result.xSize; i++)
+            {
+                for (int j = 0; j < result.ySize; j++)
+                {
+                    result.values[i, j] = 0;
+                    for (int k = 0; k < this.ySize; k++)
+                        result.values[i, j] += this.values[i, k] * matrix.values[k, j];
+                }
+            }
+            return result;
+        }
+
+        public SimpleMatrix Rotate90()
+        {
+            SimpleMatrix rotationMatrix = new SimpleMatrix(this.ySize, this.ySize);
+            rotationMatrix.SetValue(0, 0, 0);
+            rotationMatrix.SetValue(0, 1, -1);
+            rotationMatrix.SetValue(1, 0, 1);
+            rotationMatrix.SetValue(1, 1, 0);
+            return Multiply(rotationMatrix);
+        }
+
+        public void Dump(StreamWriter sw)
+        {
+            for (int i = 0; i < xSize; i++)
+            {
+                for (int j = 0; j < ySize; j++)
+                    sw.Write(String.Format("{0,2} ", values[i, j]));
+                sw.WriteLine();
+            }
+        }
+    }
+}
