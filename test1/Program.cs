6c8390d7da22510626689b978320f6fd1c51ec4a
David Shweizer <d.a.schweizer@gmail.com>
2018-10-11 13:44:22 +0200

    doet t aardig

--- test1/Program.cs
+++ test1/Program.cs
@@ -11,30 +11,45 @@ namespace test1
             StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
             sw.AutoFlush = true;
             Console.SetOut(sw);
-            TekAreaDef T1 = new TekAreaDef(new Point(3, 4));
-            T1.Dump(sw);
-            TekAreaDef T2 = new TekAreaDef(new Point(3, 4), new Point(1, 0), new Point(2, 0));
-            T2.Dump(sw);
-            TekAreaDef T3 = new TekAreaDef(new Point(3, 4), new Point(1, 0), new Point(2, 0),
-                new Point(3, 0), new Point(3, 1), new Point(4, 0)
+            //TekAreaDef T1 = new TekAreaDef(new Point(3, 4));
+            //T1.Normalized().Dump(sw, "T1");
+            //T1.FlipHorizontal().Dump(sw, "T1 flipped");
+            //TekAreaDef T2 = new TekAreaDef(new Point(3, 4), new Point(4, 4), new Point(5, 4));
+            //T2.Normalized().Dump(sw, "T2");
+            //T2.FlipHorizontal().Dump(sw, "T2 flipped");
+            TekAreaDef T3 = new TekAreaDef(new Point(3, 4), new Point(4, 4), new Point(5, 4),
+                new Point(6, 4), new Point(6, 5)
                 );
-            T3.Dump(sw);
-            TekAreaDef T4 = new TekAreaDef(new Point(3, 4), new Point(0, 1), new Point(0, 2),
-                new Point(0, 3), new Point(-1, 3), new Point(0, 4)
-                );
-            T4.Dump(sw);
+            T3.Normalized().Dump(sw, "T3");
+            T3.FlipHorizontal().Dump(sw, "T3 flipped");
+            //TekAreaDef T4 = new TekAreaDef(new Point(3, 4), new Point(3, 5), new Point(3, 6),
+            //    new Point(3, 7), new Point(2, 7)
+            //    );
+            //T4.Normalized().Dump(sw, "T4");
+            //T4.FlipHorizontal().Dump(sw, "T4 flipped");
+            //TekAreaDef T4b = T4.Normalized();
+            //T4b.Normalized().Dump(sw, "T4b");
+            //T4b.FlipHorizontal().Dump(sw, "T4b flipped");
 
-            Console.WriteLine();
-            Console.WriteLine();
-            TekAreaDef T5 = T3.Rotate90();
-            TekAreaDef T6 = T5.Rotate90();
-            TekAreaDef T7 = T6.Rotate90();
-            TekAreaDef T8 = T7.Rotate90();
-            T3.Dump(sw);
-            T5.Dump(sw);
-            T6.Dump(sw);
-            T7.Dump(sw);
-            T8.Dump(sw);
+            //Console.WriteLine();
+            
+            //TekAreaDef T5 = T3.Normalized().Rotate90();
+            //TekAreaDef T6 = T5.Rotate90();
+            //TekAreaDef T7 = T6.Rotate90();
+            //TekAreaDef T8 = T7.Rotate90();
+            //T3.Normalized().Dump(sw, "T3-normalized");
+            //T5.Dump(sw, "T5");
+            //T6.Dump(sw, "T6");
+            //T7.Dump(sw, "T7");
+            //T8.Dump(sw, "T8");
+            //Console.WriteLine();
+            //T3.Dump(sw, "T3");
+            //if (T3.Equals(T8))
+            //    sw.WriteLine("T3 equals T8");
+            //else sw.WriteLine("T3 not equal T8");
+            //if (T3.Equals(T7))
+            //    sw.WriteLine("T3 equals T7");
+            //else sw.WriteLine("T3 not equal T7");
 
 
 
--- test1/TekAreaDef.cs
+++ test1/TekAreaDef.cs
@@ -11,62 +11,213 @@ namespace test1
     public class TekAreaDef
     {
         const int MAXTEK = 5;
-        Point TopLeft;
+        List<Point> Points;
         List<Point> Deltas;
         int xSize, ySize;
 
-        public TekAreaDef(Point topLeft, params Point[] deltas)
+        private void initLists()
         {
-            TopLeft = topLeft;
             Deltas = new List<Point>();
-            foreach(Point value in deltas)
-            {
-                if (Deltas.Count == MAXTEK)
-                    break;                    
-                Deltas.Add(value);
-            }
+            Points = new List<Point>();
+        }
+
+        public TekAreaDef(TekAreaDef value)
+        {
+            initLists();
+            foreach (Point point in value.Points)
+                Points.Add(new Point(point.X, point.Y));
+            foreach (Point point in value.Deltas)
+                Deltas.Add(new Point(point.X, point.Y));
             ComputeSize();
+        }
+
+        public TekAreaDef(params Point[] values)
+        {
+            initLists();
+            foreach (Point value in values)
+            {
+                if (Points.Count == MAXTEK)
+                    throw new Exception(String.Format("Too many values for area: already {0} fields present", Points.Count));
+                Points.Add(value);
+            }
+            ComputeDeltas();
+            ComputeSize();
+        }
+
+        private void ComputeDeltas()
+        {
+            Point P = Points[0];
+            Deltas.Clear();
+            for (int i = 1; i < Points.Count; i++)
+                Deltas.Add(new Point(Points[i].X - P.X, Points[i].Y - P.Y));
         }
 
         private void ComputeSize()
         {
-            if (Deltas.Count()==0)
+            if (Points.Count() == 1)
             {
                 xSize = 1;
                 ySize = 1;
                 return;
             }
-            int xMin = MAXTEK, yMin = MAXTEK, xMax = 0, yMax = 0;
-            foreach(Point value in Deltas)
+            xSize = 1 + xMaximum() - xMinimum();
+            ySize = 1 + yMaximum() - yMinimum();
+        }
+
+        private int xMinimum()
+        {
+            int result = Int32.MaxValue;
+            foreach (Point value in Points)
             {
-                if (value.X < xMin)
-                    xMin = value.X;
-                if (value.X > xMax)
-                    xMax = value.X;
-                if (value.Y < yMin)
-                    yMin = value.Y;
-                if (value.Y > yMax)
-                    yMax = value.Y;
+                if (value.X < result)
+                    result = value.X;
             }
-            xSize = 1 + xMax - xMin;
-            ySize = 1 + yMax - yMin;
+            return result;
+        }
+
+        private int yMinimum()
+        {
+            int result = Int32.MaxValue;
+            foreach (Point value in Points)
+            {
+                if (value.Y < result)
+                    result = value.Y;
+            }
+            return result;
+        }
+
+        private int xMaximum()
+        {
+            int result = -1;
+            foreach (Point value in Points)
+            {
+                if (value.X > result)
+                    result = value.X;
+            }
+            return result;
+        }
+
+        private int yMaximum()
+        {
+            int result = -1;
+            foreach (Point value in Points)
+            {
+                if (value.Y > result)
+                    result = value.Y;
+            }
+            return result;
+        }
+
+        private int Compare(Point p1, Point p2)
+        {
+            if (p1.X < p2.X)
+                return -1;
+            else if (p1.X > p2.X)
+                return 1;
+            else if (p1.Y < p2.Y)
+                return -1;
+            else if (p1.Y > p2.Y)
+                return 1;
+            else return 0;
+        }
+
+        private void Swap(List<Point> list, int index1, int index2)
+        {
+            Point tem = new Point(list[index1].X, list[index1].Y);
+            list[index1] = list[index2];
+            list[index2] = tem;
+        }
+
+        public void Shift(int DeltaX, int DeltaY)
+        {
+            for (int i = 0; i < Points.Count; i++)
+            {
+                Points[i] = new Point(Points[i].X + DeltaX, Points[i].Y + DeltaY);
+            }
+        }
+
+        public TekAreaDef Normalized()
+        {
+            TekAreaDef result = new TekAreaDef(this);
+            for (int i = 0; i < Points.Count; i++) // simple bubblesort
+                for (int j = i + 1; j < Points.Count; j++)
+                {
+                    switch (Compare(result.Points[i], result.Points[j]))
+                    {
+                        case 1:
+                            Swap(result.Points, i, j);
+                            break;
+                        case 0:
+                        case -1: break;
+                    }
+                }
+            result.ComputeDeltas();
+            result.Shift(-result.xMinimum(), -result.yMinimum());
+            return result;
+        }
+
+        public bool Equals(TekAreaDef other)
+        {
+            TekAreaDef N1 = this.Normalized();
+            TekAreaDef N2 = other.Normalized();
+            if (N1.Points.Count != N2.Points.Count)
+                return false;
+            for (int i = 0; i < N1.Points.Count; i++)
+                if (N1.Points[i].X != N2.Points[i].X || N1.Points[i].Y != N2.Points[i].Y)
+                    return false;
+            return true;
+        }
+
+        static public StreamWriter DebugLog = new StreamWriter("c:/temp/debug.log");
+        public void Debug(string format, params object[] items)
+        {
+            DebugLog.WriteLine(format, items);
+            DebugLog.Flush();
+        }
+
+        public TekAreaDef FlipHorizontal()
+        {
+            TekAreaDef result = this.Normalized();
+            result.Dump(DebugLog, "before flip");
+            for (int i = 0; i < Points.Count / 2; i ++)
+            {                
+                int i2 = Points.Count - 1 - i;
+                Swap(result.Points, i, i2);
+                result.Dump(DebugLog, String.Format("after i = {0},  i2 = {1}", i, i2));
+            }
+            result.ComputeDeltas();
+            result.ComputeSize();
+            Debug("flipped");
+            return result;
         }
 
         public TekAreaDef Rotate90()
         {
-            TekAreaDef result = new TekAreaDef(this.TopLeft);
-            foreach(Point value in this.Deltas)
+            TekAreaDef result = new TekAreaDef(this.Points[0]);
+            foreach (Point value in this.Deltas)
             {
                 result.Deltas.Add(new Point(-value.Y, value.X));
+                Point p = result.Deltas.ElementAt(result.Deltas.Count - 1);
+                result.Points.Add(new Point(this.Points[0].X + p.X, this.Points[0].Y + p.Y));
             }
             result.ComputeSize();
-            return result;
+            return result.Normalized();
         }
 
-        public void Dump(StreamWriter sw)
+        public TekAreaDef Rotate180()
         {
-            sw.Write(String.Format("Area: [{0},{1} (size:({2},{3})]", TopLeft.X, TopLeft.Y, xSize, ySize));
-            foreach(Point value in Deltas)
+            return Rotate90().Rotate90();
+        }
+
+        public void Dump(StreamWriter sw, string msg = "")
+        {
+            sw.Write(String.Format("{0}  [(size:({1},{2})]\n\tpoints: ", msg, xSize, ySize));
+            foreach (Point value in Points)
+            {
+                sw.Write(" ({0},{1})", value.X, value.Y);
+            }
+            sw.Write("\n\tdeltas: ");
+            foreach (Point value in Deltas)
             {
                 sw.Write(" ({0},{1})", value.X, value.Y);
             }
@@ -74,59 +225,59 @@ namespace test1
         }
     }
 
-    public class SimpleMatrix
+    class TekStandardAreas
     {
-        private double[,] values;
-        public int xSize { get { return values.GetLength(0); } }
-        public int ySize { get { return values.GetLength(1); } }
+        List<TekAreaDef> values;
 
-        public double GetValue(int i, int j)
+        private void Add1FieldAreas()
         {
-            return values[i, j];
+            values.Add(new TekAreaDef(new Point(0,0)));
         }
-        public void SetValue(int i, int j, double value)
+        private void Add2FieldAreas()
         {
-            values[i, j] = value;
+            TekAreaDef newvalue = new TekAreaDef(new Point(0, 0), new Point(0, 1));
+            values.Add(newvalue);
+            values.Add(newvalue.Rotate180());
         }
-
-        public SimpleMatrix(int xSize, int ySize)
+        private void Add3FieldAreas()
         {
-            values = new double[xSize, ySize];
-        }
-
-        public SimpleMatrix Multiply(SimpleMatrix matrix)
-        {
-            SimpleMatrix result = new SimpleMatrix(this.xSize, matrix.ySize);
-            for (int i = 0; i < result.xSize; i++)
-            {
-                for (int j = 0; j < result.ySize; j++)
-                {
-                    result.values[i, j] = 0;
-                    for (int k = 0; k < this.ySize; k++)
-                        result.values[i, j] += this.values[i, k] * matrix.values[k, j];
-                }
-            }
-            return result;
+            TekAreaDef newvalue = new TekAreaDef(new Point(0, 0), new Point(0, 1), new Point(0, 2));
+            values.Add(newvalue);
+            values.Add(newvalue.Rotate180());
+            // cornered
+            newvalue = new TekAreaDef(new Point(0, 0), new Point(0, 1), new Point(1, 0));
+            values.Add(newvalue);
+            newvalue = newvalue.Rotate90();
+            values.Add(newvalue);
+            newvalue = newvalue.Rotate90();
+            values.Add(newvalue);
+            newvalue = newvalue.Rotate90();
+            values.Add(newvalue);
         }
 
-        public SimpleMatrix Rotate90()
+        private void Add4FieldAreas()
         {
-            SimpleMatrix rotationMatrix = new SimpleMatrix(this.ySize, this.ySize);
-            rotationMatrix.SetValue(0, 0, 0);
-            rotationMatrix.SetValue(0, 1, -1);
-            rotationMatrix.SetValue(1, 0, 1);
-            rotationMatrix.SetValue(1, 1, 0);
-            return Multiply(rotationMatrix);
+            // straight
+            TekAreaDef newvalue = new TekAreaDef(new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(0, 3));
+            values.Add(newvalue);
+            values.Add(newvalue.Rotate180());
+        }
+        private void Add5FieldAreas()
+        {
+            // straight
+            TekAreaDef newvalue = new TekAreaDef(new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(0, 3), new Point(0,4));
+            values.Add(newvalue);
+            values.Add(newvalue.Rotate180());
         }
 
-        public void Dump(StreamWriter sw)
+        public TekStandardAreas()
         {
-            for (int i = 0; i < xSize; i++)
-            {
-                for (int j = 0; j < ySize; j++)
-                    sw.Write(String.Format("{0,2} ", values[i, j]));
-                sw.WriteLine();
-            }
+            values = new List<TekAreaDef>();
+            Add1FieldAreas();
+            Add2FieldAreas();
+            Add3FieldAreas();
+            Add4FieldAreas();
+            Add5FieldAreas();
         }
     }
 }
